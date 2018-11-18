405de0e1aa4dc90b1e32c93484b2c0438f525674
shadowfox3735 <normanpan180@gmail.com>
2018-11-18 16:14:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,15 @@ class Song
   end
   
   def self.genre_count
-    
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1 
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
   end
   
   def self.artist_count
